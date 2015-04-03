bba720276a2e77c1c614affd6986f9795db26c4c
vtemian <vladtemian@gmail.com>
2015-04-03 22:11:40 +0300

    use NavItemLink for nav #12

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,6 +6,7 @@ import "!style!css!less!./header.less";
 import React from "react";
 import Router from "react-router";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { NavItemLink } from "react-router-bootstrap";
 let { Route, Link, RouteHandler } = Router; // eslint-disable-line
 
 export default React.createClass({
@@ -16,16 +17,19 @@ export default React.createClass({
       <Link to="dashboard">Dashboard</Link>
     </NavItem>;
 
-    return <Navbar brand="InfoEducatie">
-        <Nav ref="nav">
-            <NavItem><Link to="/">Acasa</Link></NavItem>
-            <NavItem><Link to="news">Stiri</Link></NavItem>
-            <NavItem><Link to="alumini">Alumni</Link></NavItem>
-            <NavItem><Link to="photos">Poze</Link></NavItem>
-            <NavItem><Link to="galaciuc">Galaciuc</Link></NavItem>
-            <NavItem><Link className="register-label" to="register">Inscrie-te!</Link></NavItem>
-            {this.props.isLoggedIn ? {dashboardNavItem} : null}
-        </Nav>
-    </Navbar>;
+    return <div>
+        <div id="navbar-img"></div>
+        <Navbar brand="InfoEducatie">
+            <Nav right ref="nav">
+                <NavItemLink to="/">Acasa</NavItemLink>
+                <NavItemLink to="news">Stiri</NavItemLink>
+                <NavItemLink to="alumni">Alumni</NavItemLink>
+                <NavItemLink to="photos">Poze</NavItemLink>
+                <NavItemLink to="galaciuc">Galaciuc</NavItemLink>
+                <NavItemLink className="register-label" to="register">Inscrie-te!</NavItemLink>
+                {this.props.isLoggedIn ? {dashboardNavItem} : null}
+            </Nav>
+        </Navbar>
+    </div>;
   }
 });
