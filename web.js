572ad9cb41b2ec69e558c95aeb47c7d741752ae6
Robert Dolca <robert.dolca@gmail.com>
2015-04-04 03:31:24 +0300

    Catch all URL in express
    If a file with the specified name exists it is served. If there isn't
    one we will return index.html

--- web.js
+++ web.js
@@ -1,8 +1,20 @@
+var fs = require('fs');
 var gzippo = require('gzippo');
 var logger = require('morgan');
 var express = require('express');
+
 var app = express();
+var basePath = __dirname + "/build";
+
+app.use('*', function(req, res) {
+    var filename = req.originalUrl
+
+    if (!fs.existsSync(basePath + filename))
+        filename = "/index.html";
+
+    res.sendFile(basePath + filename);
+});
 
 app.use(logger('combined'));
-app.use(gzippo.staticGzip("" + __dirname + "/build"));
+app.use(gzippo.staticGzip(basePath));
 app.listen(process.env.PORT || 5000);
