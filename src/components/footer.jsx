2ce48708f0821f2420614a726e9b3ad65c06dc54
vtemian <vladtemian@gmail.com>
2015-05-23 20:33:19 +0300

    added footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,27 @@
+"use strict";
+
+import React from "react";
+import Router from "react-router";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { NavItemLink } from "react-router-bootstrap";
+let { Route, Link, RouteHandler } = Router; // eslint-disable-line
+
+
+export default React.createClass({
+  displayName: "Footer",
+
+  render() {
+    return <div className="footer">
+      <Navbar toggleNavKey={0} className="fluid">
+        <Nav className="navbar-nav" eventKey={0} right ref="nav">
+          <NavItemLink to="alumni">Alumni</NavItemLink>
+          <NavItemLink to="forum">Forum</NavItemLink>
+          <NavItemLink to="schedule">Program</NavItemLink>
+          <NavItemLink to="results">Rezultate</NavItemLink>
+          <NavItemLink to="jury">Juriu</NavItemLink>
+          <NavItemLink to="contact">Contact</NavItemLink>
+        </Nav>
+      </Navbar>
+    </div>;
+  }
+});
