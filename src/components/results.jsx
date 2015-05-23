d28779427a9cb0a437da1bef2e1302565fd1ac21
vtemian <vladtemian@gmail.com>
2015-05-23 20:34:05 +0300

    Boilerplate for the other components

---
+++ src/components/results.jsx
@@ -0,0 +1,14 @@
+"use strict";
+
+import React from "react";
+
+
+export default React.createClass({
+  displayName: "Results",
+
+  render() {
+    return <div>
+      <h1 ref="heading">Results</h1>
+    </div>;
+  }
+});
