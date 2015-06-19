ea25456d943ded81031cff3b2d8fcd21ac4c2c71
vtemian <vladtemian@gmail.com>
2015-06-19 17:07:00 +0300

    added missing menu items

---
+++ src/components/contestants.jsx
@@ -0,0 +1,14 @@
+"use strict";
+
+import React from "react";
+
+
+export default React.createClass({
+  displayName: "Participanti",
+
+  render() {
+    return <div>
+      <h1 ref="heading">Participanți</h1>
+    </div>;
+  }
+});
