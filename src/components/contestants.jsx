e9f1aba2b4368532a56a0a477cfcbf44d9ecefb1
vtemian <vladtemian@gmail.com>
2015-06-19 18:32:34 +0300

    added middle black element

--- src/components/contestants.jsx
+++ src/components/contestants.jsx
@@ -2,13 +2,52 @@
 
 import React from "react";
 
+import { Grid, Col, Row } from "react-bootstrap";
+
+import Header from "./header";
+import "./contestants.less";
+
 
 export default React.createClass({
   displayName: "Participanti",
 
   render() {
-    return <div>
-      <h1 ref="heading">Participanți</h1>
-    </div>;
+    return <div className="contestants">
+        <div className="blue-section-wrapper">
+          <Grid className="blue-section">
+            <Header isLoggedIn={this.props.isLoggedIn} />
+            <Row className="small-spacing" />
+            <Row>
+              <Col md={12}>
+                <h1>Participanți InfoEducație Ediția 2015</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <h2>Concurs Național de Informatică</h2>
+              </Col>
+            </Row>
+            <Row className="big-spacing" />
+            <Row>
+              <Col md={6} mdOffset={3} className="middle-align hidden-xs">
+                <Row className="stats">
+                  <Col md={4} xs={2} className="right-border">
+                      <p className="description">Participanți</p>
+                      <p className="value">75</p>
+                  </Col>
+                  <Col md={4} xs={2} className="right-border">
+                      <p className="description">Proiecte</p>
+                      <p className="value">39</p>
+                  </Col>
+                  <Col md={4} xs={2} >
+                      <p className="description">Județe</p>
+                      <p className="value">17</p>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
+      </div>;
   }
 });
