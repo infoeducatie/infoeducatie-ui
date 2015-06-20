404f44abf1196e34252d59cdf89c98ac4f555452
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-06-20 19:10:25 +0300

    Refactor typography to be consistent #47

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,8 +11,8 @@ import Intel from "../../assets/img/sponsors/intel.png";
 import Orange from "../../assets/img/sponsors/orange.png";
 
 export default React.createClass({
-
   displayName: "Home",
+
   render() {
     return <div className="home">
         <div className="blue-section-wrapper">
@@ -28,33 +28,33 @@ export default React.createClass({
                 <Row>
                     <Col md={12}>
                         <h1>InfoEducație Ediția 2015</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={12}>
                         <h2>Concurs Național de Informatică</h2>
                     </Col>
                 </Row>
-                <Row className="small-spacing" />
                 <Row>
                     <Col md={12}>
-                        <p className="tagline">The best software engineering contest in the world.</p>
+                        <h5 className="tagline">The best software engineering
+                        contest in the world.</h5>
                     </Col>
                 </Row>
                 <Row className="small-spacing" />
                 <Row>
                     <Col md={4} mdOffset={2}>
                         <p className="left-button">
-                            <a href="#" className="link link-primary">Înregistrează-te</a>
+                            <a href="#" className="link link-primary">
+                                Înregistrează-te
+                            </a>
                         </p>
                     </Col>
                     <Col md={4}>
                         <p className="right-button">
-                            <a href="#" className="link link-secondary">Mai multe...</a>
+                            <a href="#" className="link link-secondary">
+                                Mai multe...
+                            </a>
                         </p>
                     </Col>
                 </Row>
-                <Row className="small-spacing" />
+                <Row className="big-spacing" />
                 <Row className="statistics">
                     <Col md={2} mdOffset={3} xs={4}>
                         <p className="description">Participanți</p>
@@ -75,51 +75,57 @@ export default React.createClass({
 
         <div className="green-section-wrapper">
             <Grid className="green-section">
-                <Row className="small-spacing" />
                 <Row className="row">
-                    <Col md={4} mdOffset={1} className="left">
-                        <h1>Știri</h1>
-                        <Row className="big-spacing" />
+                    <Col md={5} className="left">
+                        <Row className="small-spacing" />
+                        <h6>Știri</h6>
+
+                        <Row className="small-spacing" />
+
                         <p className="date">16 aprilie 2069</p>
-                        <h2>Deadline-ul de înscriere a fost prelungit.</h2>
-                        <Row className="big-spacing" />
-                        <p className="message">Deadline-ul de înscriere pentru InfoEducație Online a
-                        fost prelungit până marți 21 aprilie. Sursele trebuie
-                        trimise până joi noaptea pe Github.</p>
+                        <p className="title">Deadline-ul de înscriere a fost
+                        prelungit.</p>
+
+                        <p className="message">Deadline-ul de înscriere pentru
+                        InfoEducație Online a fost prelungit până marți 21
+                        aprilie. Sursele trebuie trimise până joi noaptea pe
+                        Github.</p>
                     </Col>
-                    <Col md={4} mdOffset={1} className="right">
-                        <Row className="big-spacing" />
+                    <Col md={5} mdOffset={1} className="right">
+                        <Row className="small-spacing" />
                         <p className="date">18 martie 2015</p>
-                        <p className="message">Au inceput inscrierile pentru InfoEducatie 2015</p>
+                        <p className="message">Au inceput inscrierile pentru
+                        InfoEducatie 2015</p>
 
                         <Row className="big-spacing" />
                         <p className="date">08 martie 2015</p>
-                        <p className="message">Website-ul InfoEducatie Online 2015 a fost
-                        actualizat</p>
+                        <p className="message">Website-ul InfoEducatie Online
+                        2015 a fost actualizat</p>
                     </Col>
                 </Row>
-                <Row className="big-spacing" />
+                <Row className="small-spacing" />
             </Grid>
         </div>
 
         <div className="gray-section-wrapper">
             <Grid className="gray-section">
-                <Row className="big-spacing" />
-                <Row className="row">
+                <Row className="small-spacing" />
+                <Row>
                     <Col md={6} mdOffset={6}>
-                        <h1>Alumnus</h1>
+                        <h6>Alumnus</h6>
                         <Row className="small-spacing" />
                         <p className="quote">La momentul respectiv părea o
                         joacă, acum infoarena este o organizație cu o
                         activitate foarte solidă și cu un impact mare în
                         rândul elevilor pasionați de informatică.</p>
                         <Row className="small-spacing" />
-                        <p className="alumnus-name">Cristian Strat</p>
-                        <p className="alumnus-position">
+                        <h2 className="alumnus-name">Cristian Strat</h2>
+                        <h6 className="alumnus-position">
                             Ex Growth Manager @ Twitter
-                        </p>
+                        </h6>
                     </Col>
                 </Row>
+                <Row className="small-spacing" />
             </Grid>
         </div>
 
@@ -127,13 +133,15 @@ export default React.createClass({
             <div className="yellow-section container-fluid">
                 <Row>
                     <Col md={4} mdOffset={2} className="text middle-align">
-                        <div>
+                        <div class="wrapper-for-flexbox">
                             <h1>Tabăra Gălăciuc</h1>
-                            <p className="data">
-                                <span className="pink-dash"></span> 2 - 8 August 2015 <span className="pink-dash"></span>
-                            </p>
-                            <p className="edition">Ediția 23</p>
-                            <Row className="big-spacing" />
+                            <h6 className="data">
+                                <span className="pink-dash"></span>
+                                2 - 8 August 2015
+                                <span className="pink-dash"></span>
+                            </h6>
+                            <h6 className="edition">Ediția 23</h6>
+                            <Row className="small-spacing" />
                             <p>
                                 <a href="#" className="link link-secondary">
                                     Mai multe poze
@@ -151,7 +159,7 @@ export default React.createClass({
                 <Row className="big-spacing" />
                 <Row>
                     <Col md={12}>
-                        <h1>Sponsori Gold</h1>
+                        <h4>Sponsori Gold</h4>
                     </Col>
                 </Row>
                 <Row>
