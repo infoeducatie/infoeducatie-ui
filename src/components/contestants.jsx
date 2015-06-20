6f94f3761ae01cafb46b94689f1168c26f0f3196
vtemian <vladtemian@gmail.com>
2015-06-20 20:01:45 +0300

    Implement project card

--- src/components/contestants.jsx
+++ src/components/contestants.jsx
@@ -2,8 +2,7 @@
 
 import React from "react";
 
-import { Grid, Col, Row } from "react-bootstrap";
-
+import { Grid, Col, Row, Glyphicon } from "react-bootstrap";
 import Header from "./header";
 import "./contestants.less";
 import "./jury.less";
@@ -49,24 +48,77 @@ export default React.createClass({
             </Row>
           </Grid>
         </div>
-        <Row className="small-spacing" />
+        <Row className="big-spacing" />
         <div className="white-section-wrapper">
           <Grid>
             <Row className="white-section">
-              <Col md={1}>
+              <Col mdOffset={2} md={1}>
                 <div className="round-icon"><span className="educational"></span></div>
               </Col>
               <Col md={1}>
-                <div className="round-icon"><span className="media"></span></div>
+                <div className="round-icon"><span className="media" /></div>
               </Col>
               <Col md={1}>
-                <div className="round-icon"><span className="robots"></span></div>
+                <div className="round-icon"><span className="robots" /></div>
               </Col>
               <Col md={1}>
-                <div className="round-icon"><span className="utility"></span></div>
+                <div className="round-icon"><span className="utility" /></div>
               </Col>
               <Col md={1}>
-                <div className="round-icon"><span className="web"></span></div>
+                <div className="round-icon"><span className="web" /></div>
+              </Col>
+              <Col mdOffset={2} md={1}>
+                <Glyphicon glyph='th-large' className="icon" />
+              </Col>
+              <Col md={1}>
+                <Glyphicon glyph='align-justify' className="icon" />
+              </Col>
+            </Row>
+          </Grid>
+          <Grid>
+            <Row className="white-section">
+              <Col mdOffset={1} md={8}>
+                <div className="item">
+                  <div className="participant-container">
+                    <Row className="big-spacing"></Row>
+                    <Row className="project-title">Time Travel</Row>
+                    <Row className="big-spacing"></Row>
+                    <Row className="project-details">
+                      <Col md={12}>
+                        <div className="author">claudia rujoiu</div>
+                        <div className="author">dan stoian</div>
+                        <Row className="large-spacing"></Row>
+                        <Row>
+                          <Col md={1}>
+                            <Glyphicon inverse glyph='map-marker' className="icon" />
+                          </Col>
+                          <Col md={6}>
+                            hunedoara
+                          </Col>
+                          <Col md={1} mdOffset={2}>
+                            7
+                          </Col>
+                        </Row>
+                        <Row className="big-spacing"></Row>
+                      </Col>
+                    </Row>
+                  </div>
+                  <Row className="orange-section">
+                    <Col md={12} >
+                      <Row className="big-spacing"></Row>
+                      <Row>
+                        <Col md={4} mdOffset={1}>
+                          utilitar
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </div>
+                <div className="item">
+                  <div className="well">
+                  1 blah blah blah
+                  </div>
+                </div>
               </Col>
             </Row>
           </Grid>
