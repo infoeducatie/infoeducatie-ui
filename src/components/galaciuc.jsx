9da60042d549d42d8def594866e997cb3819ca00
vtemian <vladtemian@gmail.com>
2015-06-22 21:15:05 +0300

    Add title and initial blue-section #40

--- src/components/galaciuc.jsx
+++ src/components/galaciuc.jsx
@@ -2,22 +2,36 @@
 
 import React from "react";
 
+import { Row, Col, Grid } from "react-bootstrap";
+import Header from "./header";
+
+import "./galaciuc.less"
+
 
 export default React.createClass({
   displayName: "GalaciucPage",
 
   render() {
-    return <div>
-      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
-      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
-      <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-      <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h6>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <p><a href="#" className="link link-primary">Link Primary</a></p>
-      <p><a href="#" className="link link-secondary">Link Secondary</a></p>
-      <p><a href="#" className="link link-ternary">Link Ternary</a></p>
-    </div>;
+    return <div className="galaciuc">
+        <div className="blue-section-wrapper">
+            <Grid className="blue-section">
+                <Row>
+                    <Col md={12}>
+                        <Header isLoggedIn={this.props.isLoggedIn}
+                                login={this.props.login}
+                                logout={this.props.logout} />
+                    </Col>
+                </Row>
+                <Row className="small-spacing header-spacing" />
+                <Row>
+                    <Col md={12}>
+                        <h1>Despre Concursul InfoEducație</h1>
+                        <h2>istoric și regulament</h2>
+                    </Col>
+                </Row>
+                <Row className="small-spacing header-spacing" />
+            </Grid>
+        </div>
+      </div>;
   }
 });
