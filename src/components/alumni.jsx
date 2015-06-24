266ad4724f74ecf6f1cbcc561f049f4892882790
vtemian <vladtemian@gmail.com>
2015-06-24 18:41:10 +0300

    Add basic structure for alumni page. #54

--- src/components/alumni.jsx
+++ src/components/alumni.jsx
@@ -1,14 +1,36 @@
 "use strict";
 
 import React from "react";
+import {Grid, Row, Col} from "react-bootstrap";
+
+import Header from "./header";
+
+import "./alumni.less";
 
 
 export default React.createClass({
   displayName: "Alumni",
 
-  render() {
-    return <div>
-      <h1 ref="heading">Alumni</h1>
+   render() {
+     return <div className="home">
+      <div className="blue-section-wrapper">
+        <Grid className="blue-section">
+          <Row>
+            <Col md={12}>
+              <Header isLoggedIn={this.props.isLoggedIn}
+                      login={this.props.login}
+                      logout={this.props.logout} />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+              <h1>Alumni InfoEducație</h1>
+              <h2>Generația IT din România</h2>
+            </Col>
+          </Row>
+          <Row className="big-spacing" />
+        </Grid>
+      </div>
     </div>;
-  }
+   }
 });
