a1d06a731db70d8cfbbf27a0357395c774d22df7
vtemian <vladtemian@gmail.com>
2015-06-24 20:52:46 +0300

    Build alumnus container. #54

--- src/components/alumni.jsx
+++ src/components/alumni.jsx
@@ -1,29 +1,73 @@
 "use strict";
 
 import React from "react";
+
+import ctx from "classnames";
 import {Grid, Row, Col} from "react-bootstrap";
 
 import Header from "./header";
 
 import "./alumni.less";
+import CristiAvatar from "../../assets/img/cristi.png";
 
 
 export default React.createClass({
   displayName: "Alumni",
 
-   render() {
-     return <div className="home">
+  getInitialState() {
+    return {
+      alumni: [
+        {
+          "description": "La momentul respectiv, era o joacă, acum infoarena este o organizație cu activitate solidă și cu un impact mare în randul tinerilor pasionați de informatică.",
+          "avatar": {CristiAvatar},
+          "name": "Cristian Strat",
+          "position": "growth @ twitter",
+          "color": "green"
+        }
+      ]
+    }
+  },
+
+  renderAlumnus(alumnus) {
+    let className = ctx("alumnus-container", alumnus.color);
+
+    return <Row>
+      <Col mdOffset={3} md={6}>
+        <Row className="small-spacing" />
+        <Row>
+          <Col className={className}>
+            <Row className="small-spacing" />
+            <Row>
+              <Col mdOffset={1} md={1}>
+                <Row className="small-spacing" />
+                <div className="alumnus-image"><img src={alumnus.avatar} /></div>
+              </Col>
+              <Col mdOffset={1} md={8}>
+                <p>{alumnus.description}</p>
+                <p className="alumnus-name">{alumnus.name}</p>
+                <p className="alumnus-position">{alumnus.position}</p>
+              </Col>
+            </Row>
+            <Row className="small-spacing" />
+          </Col>
+        </Row>
+      </Col>
+    </Row>;
+  },
+
+  render() {
+    return <div className="alumni">
       <div className="blue-section-wrapper">
         <Grid className="blue-section">
           <Row>
-            <Col md={12}>
+            <Col>
               <Header isLoggedIn={this.props.isLoggedIn}
                       login={this.props.login}
                       logout={this.props.logout} />
             </Col>
           </Row>
           <Row>
-            <Col md={12}>
+            <Col>
               <h1>Alumni InfoEducație</h1>
               <h2>Generația IT din România</h2>
             </Col>
@@ -31,6 +75,9 @@ export default React.createClass({
           <Row className="big-spacing" />
         </Grid>
       </div>
-    </div>;
-   }
+      <Grid>
+        {this.state.alumni.map(this.renderAlumnus)}
+      </Grid>
+   </div>;
+  }
 });
