fc7832eb37caa0b494ed94a54b9e5aaec32aaf75
vtemian <vladtemian@gmail.com>
2015-06-27 23:53:46 +0300

    Add filter icons from contestants. #43

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Grid, Col, Row, Glyphicon } from "react-bootstrap";
 
 import Header from "./header";
+import FilterIcon from "./contestants/filter_icon";
 
 import "./results.less";
 
@@ -11,6 +12,21 @@ import "./results.less";
 export default React.createClass({
   displayName: "Results",
 
+  getInitialState: function() {
+    return {
+      currentCategory: "all"
+    }
+  },
+
+  toggleCategory(category) {
+    if (this.state.currentCategory === category) {
+      category = "all";
+    }
+
+    this.setState({
+      currentCategory: category
+    });
+  },
 
   render() {
     return <div className="results">
@@ -31,6 +47,37 @@ export default React.createClass({
           <Row className="big-spacing" />
         </Grid>
       </div>
+
+      <Grid>
+        <Row className="small-spacing" />
+        <Row className="filter-buttons">
+          <Col smOffset={3} sm={1} xs={2} xsOffset={1}>
+            <FilterIcon currentCategory={this.state.currentCategory}
+                        toggleCategory={this.toggleCategory}
+                        category="educational" />
+          </Col>
+          <Col sm={1} xs={2}>
+            <FilterIcon currentCategory={this.state.currentCategory}
+                        toggleCategory={this.toggleCategory}
+                        category="media" />
+          </Col>
+          <Col sm={1} xs={2}>
+            <FilterIcon currentCategory={this.state.currentCategory}
+                        toggleCategory={this.toggleCategory}
+                        category="robots" />
+          </Col>
+          <Col sm={1} xs={2}>
+            <FilterIcon currentCategory={this.state.currentCategory}
+                        toggleCategory={this.toggleCategory}
+                        category="utility" />
+          </Col>
+          <Col sm={1} xs={2}>
+            <FilterIcon currentCategory={this.state.currentCategory}
+                        toggleCategory={this.toggleCategory}
+                        category="web" />
+          </Col>
+        </Row>
+      </Grid>
     </div>;
   }
 });
