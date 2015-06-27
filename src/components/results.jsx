f248b0605dd133e92a7dc00d204e32c33ba4d947
vtemian <vladtemian@gmail.com>
2015-06-28 00:09:34 +0300

    Import testing projects. #43

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -14,18 +14,57 @@ export default React.createClass({
 
   getInitialState: function() {
     return {
-      currentCategory: "all"
+      currentCategory: "educational",
+      results: {
+        web: [
+          { rank: "I",
+            project: {
+              id: 1,
+              name: "Quizrl",
+              authors: [{id: 1, name: "Lacătușu Casian"}],
+              school: "Colegiul Național Traian",
+              score: 68.43,
+              open: 78.96,
+              total: 73.70
+            }
+          },
+          { rank: "II",
+            project: {
+              id: 2,
+              name: "Nexus Play",
+              authors: [{id: 2, name: "Cristian Alexandru"},
+                        {id: 3, name: "Ghinea Diana-Elena"}],
+              school: "Colegiul Național de Informatică „Tudor Vianu”",
+              score: 68.21,
+              open: 78.11,
+              total: 73.16
+            }
+          },
+          { rank: "III",
+            project: {
+              id: 3,
+              name: "Locatia Mea",
+              authors: [{id: 4, name: "Cozloschi Florin"}],
+              school: "Liceul Teoretic 'Traian Vuia' Faget",
+              score: 67.93,
+              open: 75.54,
+              total: 71.74
+            }
+          }
+        ]
+      }
     };
   },
 
   toggleCategory(category) {
-    if (this.state.currentCategory === category) {
-      category = "all";
-    }
-
     this.setState({
       currentCategory: category
     });
+  },
+
+  renderTable() {
+    return <Grid>
+    </Grid>;
   },
 
   render() {
@@ -78,6 +117,7 @@ export default React.createClass({
           </Col>
         </Row>
       </Grid>
+      {this.renderTable()}
     </div>;
   }
 });
