e5b3c40b6db020b6a6dc2892af2b0df582f86eb2
vtemian <vladtemian@gmail.com>
2015-06-27 23:44:18 +0300

    Add basic results page structure. #43

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,14 +1,36 @@
 "use strict";
 
 import React from "react";
+import { Grid, Col, Row, Glyphicon } from "react-bootstrap";
+
+import Header from "./header";
+
+import "./results.less";
 
 
 export default React.createClass({
   displayName: "Results",
 
+
   render() {
-    return <div>
-      <h1 ref="heading">Results</h1>
+    return <div className="results">
+      <div className="blue-section-wrapper">
+        <Grid className="blue-section">
+          <Header isLoggedIn={this.props.isLoggedIn} />
+          <Row className="xsmall-spacing" />
+          <Row>
+            <Col>
+              <h1>Rezultate InfoEducație</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2>Concurs Național de Informatică</h2>
+            </Col>
+          </Row>
+          <Row className="big-spacing" />
+        </Grid>
+      </div>
     </div>;
   }
 });
