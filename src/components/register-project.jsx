9ba9d615d77431ae0827b79fc6b4aaced65074e7
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-06-28 12:12:59 +0300

    Add scaffold for register page #78

--- src/components/register-project.jsx
+++ src/components/register-project.jsx
@@ -1,23 +1,31 @@
 "use strict";
 
 import React from "react";
+import { Grid, Col, Row } from "react-bootstrap";
 
+import Header from "./header";
+import "./register-project.less";
 
 export default React.createClass({
   displayName: "RegisterProjectPage",
 
   render() {
-    return <div>
-      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
-      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
-      <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-      <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h6>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <p><a href="#" className="link link-primary">Link Primary</a></p>
-      <p><a href="#" className="link link-secondary">Link Secondary</a></p>
-      <p><a href="#" className="link link-ternary">Link Ternary</a></p>
+    return <div className="register-project">
+      <div className="blue-section-wrapper">
+        <Grid className="blue-section">
+          <Header isLoggedIn={this.props.isLoggedIn} />
+          <Row>
+            <Col>
+              <h1>Înregistrează un proiect</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2>Te rugăm să completezi acest formular cu grijă</h2>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     </div>;
   }
 });
