448c235f4d7da7a9ac0d4a7ee9bde1f172f4adcf
vtemian <vladtemian@gmail.com>
2015-06-30 01:56:11 +0300

    Implement a component which render the pdf using pdfjs

---
+++ src/components/pdf-js.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import "pdfjs-dist/build/pdf";
+import "pdfjs-dist/web/pdf_viewer";
+
+import PDFJSWorker from "pdfjs-dist/build/pdf.worker.js";
+
+
+export default React.createClass({
+  getInitialState() {
+    return {};
+  },
+  componentDidMount() {
+    let self = this;
+
+    console.log(PDFJSWorker);
+    PDFJS.workerSrc = PDFJSWorker;
+    PDFJS.getDocument(this.props.file).then(function(pdf) {
+      pdf.getPage(self.props.page).then(function(page) {
+        self.setState({pdfPage: page, pdf: pdf});
+      });
+    });
+  },
+  componentWillReceiveProps(newProps) {
+    let self = this;
+    if (newProps.page) {
+      self.state.pdf.getPage(newProps.page).then(function(page) {
+        self.setState({pdfPage: page, pageId: newProps.page});
+      });
+    }
+    this.setState({
+      pdfPage: null
+    });
+  },
+  getDefaultProps() {
+    return {page: 1};
+  },
+  render() {
+    let self = this;
+    if (this.state.pdfPage) setTimeout(function() {
+      let canvas = self.getDOMNode(),
+          context = canvas.getContext('2d'),
+          scale = self.props.scale || 1.0,
+          viewport = self.state.pdfPage.getViewport(scale);
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      let renderContext = {
+        canvasContext: context,
+        viewport: viewport
+      };
+      self.state.pdfPage.render(renderContext);
+    });
+    return this.state.pdfPage ? <canvas></canvas> : <div>Loading pdf..</div>;
+  }
+});
