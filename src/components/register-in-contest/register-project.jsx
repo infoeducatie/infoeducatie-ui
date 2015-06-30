da0e149b4cd07588201eea969349b76c9c721dab
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-07-01 00:08:16 +0300

    Full stack fix for category #33

--- src/components/register-in-contest/register-project.jsx
+++ src/components/register-in-contest/register-project.jsx
@@ -22,7 +22,7 @@ export default React.createClass({
       project: {
         /*eslint-disable */
         title: "",
-        category: "web",
+        category_name: "web",
         description: "",
         technical_description: "",
         system_requirements: "",
@@ -43,7 +43,7 @@ export default React.createClass({
         required />
       <Input type="select"
              label="Categorie"
-             valueLink={this.deepLinkState(["project", "category"])}
+             valueLink={this.deepLinkState(["project", "category_name"])}
              required>
         <option value="educational">Software Educațional</option>
         <option value="utilitar">Software Utilitar</option>
