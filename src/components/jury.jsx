41e9a26efdd2a1454564b5a6ff8372c5796f54b7
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-06-30 21:25:18 +0300

    Remove entire authentication #78

--- src/components/jury.jsx
+++ src/components/jury.jsx
@@ -5,7 +5,6 @@ import { Grid, Col, Row } from "react-bootstrap";
 
 import Header from "./header";
 import JuryDescription from "./jury-description";
-import Authentication from "../mixins/authentication";
 
 import "./jury.less";
 
--- src/mixins/authentication.jsx
+++ src/mixins/authentication.jsx
@@ -1,16 +1,1 @@
-"use strict";
-
-/**
- * Check if user is authenticate, or redirect him to /.
- */
-export default {
-  statics: {
-    willTransitionTo: function(transition) {
-      var nextPath = transition.path;
-      if (!window.Auth.isLoggedIn()) {
-        transition.redirect("/", {},
-          {"nextPath": nextPath});
-      }
-    }
-  }
-};
+// TODO @palcu: add this when react router arrives to 1.0
