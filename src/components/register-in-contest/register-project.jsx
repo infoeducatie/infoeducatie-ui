ae4bdb8a90778ac54a76b7a6b148dcf5498f0253
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-07-01 00:14:54 +0300

    Forgotten import #33
    How the fuck did this work? I am too tired.

--- src/components/register-in-contest/register-project.jsx
+++ src/components/register-in-contest/register-project.jsx
@@ -2,6 +2,7 @@
 
 import DeepLinkedStateMixin from "react-deep-link-state";
 import React from "react";
+import _ from "lodash";
 import { Input, ButtonInput } from "react-bootstrap";
 
 import FormMixin from "../../mixins/form"
