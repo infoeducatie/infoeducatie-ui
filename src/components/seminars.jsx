e20141cc688e12d73f0507c0958488fd4e3f1df9
vtemian <vladtemian@gmail.com>
2015-07-01 23:34:18 +0300

    Add two more seminars example

--- src/components/seminars.jsx
+++ src/components/seminars.jsx
@@ -8,7 +8,9 @@ import {Grid, Row, Col} from "react-bootstrap";
 import Header from "./header";
 
 import "./seminars.less";
-import CristiAvatar from "../../assets/img/alumni/cristi.png";
+import BuragaAvatar from "../../assets/img/seminars/sabin_buraga.jpg";
+import BogdanAvatar from "../../assets/img/seminars/bogdan_gaza.jpg";
+import ViviAvatar from "../../assets/img/alumni/vivi.png";
 
 
 export default React.createClass({
@@ -18,15 +20,61 @@ export default React.createClass({
     return {
       seminars: [
         {
-          "description": `O introducere in conceptele necesare dezvoltarii aplicatilor bazate pe paradigma pe care cloud computing o introduce. Putem la discutie functionalitatile, sabloanele de dezvoltare si constrangerile pe care o platforma de tip cloud le ofera. Discutia va fi in jurul servicilor puse la dispozitie de Amazon Web Services. `,
+          "description": `O introducere în conceptele necesare dezvoltării
+                          aplicaților bazate pe paradigma pe care cloud
+                          computing o introduce. Punem la discuție
+                          funcționalitățile, șabloanele de dezvoltare și
+                          constrângerile pe care o platformă de tip cloud
+                          le oferă. Discuția va fi în jurul servicilor puse la
+                          dispoziție de Amazon Web Services.`,
           "lector": {
-            "avatar": CristiAvatar,
-            "name": "Bogdan Gaza",
+            "avatar": BogdanAvatar,
+            "name": "Bogdan Gâza",
             "position": "software engineer @ twitter"
           },
           "title": "Architecting for the cloud",
           "color": "green"
         },
+        {
+          "description": `Ma numesc Octavian Costache. In Romania am fost
+                          fondatorul doizece.ro si didactic.ro. Am vandut
+                          doizece.ro si m-am mutat in NY, unde am lucrat la
+                          Google timp de cinci ani (timp in care am construit
+                          produse populare si apreciate ca Google Finance, un
+                          feature in Gmail numit Multiple Inboxes, feature-ul
+                          de bookmarking pe Google Maps). Acum sunt fondatorul
+                          unui startup in NY. Mi-ar face placere sa discutam
+                          despre viata si lumea startup-urilor, cum sa
+                          construiesti un produs, cum a fost viata la Google,
+                          cum este viata dupa Google, sau de ce Infoeducatia a
+                          fost unul din evenimentele formative din viata si din
+                          cariera mea de pana acum. Orice intrebari despre
+                          Google, New York, startups, software development sunt
+                          binevenite si mi-ar placea sa incerc sa lamuresc
+                          orice curiozitate sau nelamuriri aveti despre orice. `,
+          "lector": {
+            "avatar": ViviAvatar,
+            "name": "Octavian Costache",
+            "position": "founder & cto @ spring"
+          },
+          "title": "The answer to life, the universe and everything",
+          "color": "orange"
+        },
+        {
+          "description": `Sub titlul "O lectie de anatomie Web. Disectia unui
+                          document HTML" se ascunde o prezentare referitoare
+                          la posibilitatea de a selecta/accesa parti dintr-o
+                          pagina Web. Vom descrie cei mai interesanti selectori
+                          CSS3, vom relata ce inseamna limbajul XPath si vom
+                          aplica putina programare. `,
+          "lector": {
+            "avatar": BuragaAvatar,
+            "name": "Dr. Sabin Buraga",
+            "position": "Profesor @ universitatea \"alexandru ioan cuza\""
+          },
+          "title": "O lectie de anatomie Web",
+          "color": "black"
+        }
       ]
     };
   },
@@ -35,26 +83,27 @@ export default React.createClass({
     let className = ctx("seminar-container", seminar.color);
 
     return <Row key={seminar.id}>
-      <Col mdOffset={2} md={8} smOffset={1} sm={10}>
-        <Row className="small-spacing" />
+      <Col mdOffset={2} md={8} smOffset={1} sm={10} xs={12}>
+        <Row className="xsmall-spacing" />
         <Row>
           <Col className={className} xs={12}>
-            <Row className="small-spacing" />
+            <Row className="xsmall-spacing" />
             <Row>
               <Col xs={3} xsOffset={1}>
-                <Row className="xsmall-spacing" />
+                <Row className="small-spacing" />
                 <div className="seminar-image">
                   <img src={seminar.lector.avatar} />
                 </div>
               </Col>
-              <Col xs={7} xsOffset={1}>
+              <Col xs={8} >
+                <h4 className="seminar-title">{seminar.title}</h4>
                 <p>{seminar.description}</p>
-                <Row className="small-spacing" />
+                <Row className="xsmall-spacing" />
                 <h5 className="seminar-name">{seminar.lector.name}</h5>
                 <p className="seminar-position">{seminar.lector.position}</p>
               </Col>
             </Row>
-            <Row className="small-spacing" />
+            <Row className="xsmall-spacing" />
           </Col>
         </Row>
       </Col>
