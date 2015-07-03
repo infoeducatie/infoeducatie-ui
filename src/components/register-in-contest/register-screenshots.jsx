f643f6e42d40fe05cd367b689c87f2551261de9e
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-07-03 21:08:17 +0300

    Add screenshot tentative implementation 2

---
+++ src/components/register-in-contest/register-screenshots.jsx
@@ -0,0 +1,36 @@
+"use strict";
+
+import $ from "jquery";
+import React from "react";
+import _ from "lodash";
+import DeepLinkedStateMixin from "react-deep-link-state";
+import { ButtonInput, Input } from "react-bootstrap";
+
+import FormMixin from "../../mixins/form"
+
+require("blueimp-file-upload");
+
+export default React.createClass({
+  displayName: "RegisterFinish",
+
+  componentDidMount() {
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        headers: {
+          Authorization: this.props.access_token
+        },
+        add: function (e, data) {
+            data.context = $('<p/>').text('Uploading...').appendTo(document.body);
+            data.submit();
+        },
+        done: function (e, data) {
+            data.context.text('Upload finished.');
+        }
+    });
+  },
+
+  render() {
+    let url = window.config.API_URL + this.props.formEndpoint;
+    return <input id="fileupload" type="file" name="files[]" data-url={url} multiple />
+  }
+});
