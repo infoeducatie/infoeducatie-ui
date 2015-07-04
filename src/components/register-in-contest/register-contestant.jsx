0f79f76dd1b3aba8a7426c1db5974fdc21eacad1
vtemian <vladtemian@gmail.com>
2015-07-05 00:59:45 +0300

    Add counties. #90

--- src/components/register-in-contest/register-contestant.jsx
+++ src/components/register-in-contest/register-contestant.jsx
@@ -65,12 +65,54 @@ export default React.createClass({
         label="Țara *"
         onChange={this.onCountryChange}
         required />
-      <Input
-        type="text"
-        placeholder="Vrancea"
-        onChange={this.onCountyChange}
-        label="Județ *"
-        required />
+      <Input type="select"
+             placeholder="Vrancea"
+             onChange={this.onCountyChange}
+             label="Județ *"
+             required>
+        <option value="Alba">Alba</option>
+        <option value="Arad">Arad</option>
+        <option value="Argeș">Argeș</option>
+        <option value="Bacău">Bacău</option>
+        <option value="Bihor">Bihor</option>
+        <option value="Bistrița Năsăud">Bistrița Năsăud</option>
+        <option value="Botoșani">Botoșani</option>
+        <option value="Brașov">Brașov</option>
+        <option value="Brăila">Brăila</option>
+        <option value="București">București</option>
+        <option value="Buzău">Buzău</option>
+        <option value="Caraș Severin">Caraș Severin</option>
+        <option value="Călărași">Călărași</option>
+        <option value="Cluj">Cluj</option>
+        <option value="Constanța">Constanța</option>
+        <option value="Covasna">Covasna</option>
+        <option value="Dâmbovița">Dâmbovița</option>
+        <option value="Dolj">Dolj</option>
+        <option value="Galați">Galați</option>
+        <option value="Giurgiu">Giurgiu</option>
+        <option value="Gorj">Gorj</option>
+        <option value="Harghita">Harghita</option>
+        <option value="Hunedoara">Hunedoara</option>
+        <option value="Ialomița">Ialomița</option>
+        <option value="Iași">Iași</option>
+        <option value="Ilfov">Ilfov</option>
+        <option value="Maramureș">Maramureș</option>
+        <option value="Mehedinți">Mehedinți</option>
+        <option value="Mureș">Mureș</option>
+        <option value="Neamț">Neamț</option>
+        <option value="Olt">Olt</option>
+        <option value="Prahova">Prahova</option>
+        <option value="Satu Mare">Satu Mare</option>
+        <option value="Sălaj">Sălaj</option>
+        <option value="Sibiu">Sibiu</option>
+        <option value="Suceava">Suceava</option>
+        <option value="Teleorman">Teleorman</option>
+        <option value="Timiș">Timiș</option>
+        <option value="Tulcea">Tulcea</option>
+        <option value="Vaslui">Vaslui</option>
+        <option value="Vâlcea">Vâlcea</option>
+        <option value="Vrancea">Vrancea</option>
+      </Input>
       <Input
         type="text"
         placeholder="Gălăciuc"
