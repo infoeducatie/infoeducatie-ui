2a04bfe2417797a112834fce069861f7401c8ec1
vtemian <vladtemian@gmail.com>
2015-07-04 14:38:37 +0300

    Add news fixture

---
+++ src/fixtures/news.js
@@ -0,0 +1,59 @@
+export default [
+  { "title": "Anul acesta InfoEducație aaaa",
+    "small_description": `Lista cu prezentările online programate poate fi
+                          gasită pe pagina de calendar. Inregistrarile
+                          sunt disponibile pe YouTube.`,
+    "body": `Lista cu prezentările online programate poate fi gasită pe
+             pagina de calendar. Inregistrarile sunt disponibile pe
+             YouTube. La momentul respectiv părea o joacă, acum infoarena
+             este o organizație cu o activitate foarte solidă și cu un
+             impact mare în rândul elevilor pasionați de informatică.`,
+    "date": "28 iunie 2015"
+  },
+  {
+    "title": "Anul acesta InfoEducație aaaa",
+    "small_description": `Lista cu prezentările online programate poate fi
+                          gasită pe pagina de calendar. Inregistrarile
+                          sunt disponibile pe YouTube.`,
+    "body": null,
+    "date": "04 mai 2015"
+  },
+  {
+    "title": "Anul acesta InfoEducație aaaa",
+    "small_description": `Lista cu prezentările online programate poate fi
+                          gasită pe pagina de calendar. Inregistrarile
+                          sunt disponibile pe YouTube.`,
+    "body": `Lista cu prezentările online programate poate fi gasită pe
+             pagina de calendar. Inregistrarile sunt disponibile pe
+             YouTube. La momentul respectiv părea o joacă, acum infoarena
+             este o organizație cu o activitate foarte solidă și cu un
+             impact mare în rândul elevilor pasionați de informatică.`,
+    "date": "28 aprilie 2015"
+  },
+  {
+    "title": "Merge paginarea",
+    "small_description": `Deadline-ul de înscriere pentru InfoEducație
+                          Online a fost prelungit până marți, 21
+                          aprilie. Sursele trebuie trimise până joi, 23
+                          aprilie, via email către juriu (link github
+                          sau arhivă).`,
+    "body": `Lista cu prezentările online programate poate fi gasită pe
+             pagina de calendar. Inregistrarile sunt disponibile pe
+             YouTube. La momentul respectiv părea o joacă, acum infoarena
+             este o organizație cu o activitate foarte solidă și cu un
+             impact mare în rândul elevilor pasionați de informatică.`,
+    "date": "24 aprilie 2015"
+  },
+  {
+    "title": "Tot merge paginarea",
+    "small_description": `Lista cu prezentările online programate poate fi
+                          gasită pe pagina de calendar. Inregistrarile
+                          sunt disponibile pe YouTube.`,
+    "body": `Lista cu prezentările online programate poate fi gasită pe
+             pagina de calendar. Inregistrarile sunt disponibile pe
+             YouTube. La momentul respectiv părea o joacă, acum infoarena
+             este o organizație cu o activitate foarte solidă și cu un
+             impact mare în rândul elevilor pasionați de informatică.`,
+    "date": "28 februarie 2015"
+  }
+];
