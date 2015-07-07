b8f576af49966a168730b76f8610f944108a6fd1
Robert Dolca <robert.dolca@gmail.com>
2015-07-07 23:28:00 +0300

    Auto release/tag deployment

---
+++ autodeply.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tag=`git tag | egrep "^v[0-9.]*$" | sort | tail -n 1`
+
+if [ -z $tag ]; then
+  echo "No tag found"
+  exit 1
+fi
+
+if [ `git show-ref $tag | cut -d ' ' -f1` == `git rev-parse HEAD` ]; then
+  echo "Already the latest version"
+  exit 0
+fi
+
+echo "Deploying tag ${tag}"
+
+git reset --hard $tag
+npm run build
