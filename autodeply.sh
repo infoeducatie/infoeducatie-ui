343f361de860169954d4731dc0429f6127303b7d
Robert Dolca <robert.dolca@gmail.com>
2015-07-13 22:43:42 +0300

    Fix autodeploy script tag sorting

--- autodeply.sh
+++ autodeply.sh
@@ -2,7 +2,7 @@
 # This script should be periodicly run bya cron
 
 git fetch &> /dev/null
-tag=`git tag | egrep "^v[0-9.]*$" | sort | tail -n 1`
+tag=`git tag | egrep "^v[0-9.]*$" | sort -t '.' -n -k1,1 -k2,2 -k3,3 -k4,4 -k5,5 | tail -n 1`
 
 if [ -z $tag ]; then
   echo "No tag found"
