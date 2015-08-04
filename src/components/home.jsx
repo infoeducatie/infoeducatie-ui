1d853ae8a7e42c0789358e550f46cf9d9297131a
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-08-04 12:46:23 +0300

    Add sigle GInfo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -25,6 +25,7 @@ import eSkills from "../../assets/img/sponsors/eskills.png";
 import gwc from "../../assets/img/sponsors/girlswhocode.png";
 import leonte from "../../assets/img/sponsors/leonte.png";
 import altex from "../../assets/img/sponsors/altex.gif";
+import GInfo from "../../assets/img/sponsors/ginfo.png";
 
 
 export default React.createClass({
@@ -243,6 +244,9 @@ export default React.createClass({
                             </a>
                             <a href="http://www.edu.ro/" target="_blank">
                               <img src={MEN} />
+                            </a>
+                            <a href="http://gazeta.info.ro/" target="_blank">
+                              <img src={GInfo} />
                             </a>
                         </p>
                     </Col>
