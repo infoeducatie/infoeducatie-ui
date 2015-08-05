ef8aa640da50ea7def5a15a0fdfbcd7506882619
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-08-05 15:07:24 +0300

    Missing comma

--- src/components/jury.jsx
+++ src/components/jury.jsx
@@ -69,7 +69,7 @@ export default React.createClass({
         {"avatar": DefaultAvatar, "name": "Maria Stan", "occupation": "Profesor @ Colegiul Tehnic 'Radu Negru' Galati"},
         {"avatar": DefaultAvatar, "name": "Rodica Pintea", "occupation": "Profesor @ Liceul 'Grigore Moisil' Bucureşti"},
         {"avatar": DefaultAvatar, "name": "Ovidiu Tirian", "occupation": "Sef lucrari @ Universitatea Politehnică Timișoara - Inginerie Hunedoara"},
-        {"avatar": DefaultAvatar, "name": "Mircea Bratan", "occupation": "Inginer Software @ Tremend Software Consulting"}
+        {"avatar": DefaultAvatar, "name": "Mircea Bratan", "occupation": "Inginer Software @ Tremend Software Consulting"},
         {"avatar": DefaultAvatar, "name": "Adriana Cherecheș", "occupation": "Profesor @ Liceul Teoretic Nicolae Balcescu"}
     ];
     var utilitar = [
