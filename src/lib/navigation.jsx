a97e2125daf1d524865c3c5440b00a988dd34690
Victor Sandu <vyctor661@gmail.com>
2026-08-01 14:21:12 +0300

    Avoid blocker-sensitive local module names

--- src/lib/navigation.jsx
+++ src/lib/navigation.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-import { trackPageView } from "./analytics";
+import { trackPageView } from "./telemetry";
 
 let navigateWithRouter = null;
 
